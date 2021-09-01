383e6000db509037d4d449e46795b20c87a1b1c9
godingly <godingly@gmail.com>
2021-09-01 09:49:26 +0300

    minimize

--- src/main.py
+++ src/main.py
@@ -6,11 +6,11 @@ import sys
 def parse_args():
     parser = argparse.ArgumentParser()
     
-    parser.add_argument('gcal_credentials_json', 
+    parser.add_argument('--gcal_credentials_json', 
                         help='path of google calendar credentials.json, https://developers.google.com/workspace/guides/create-credentials')
-    parser.add_argument('beeminder_json', 
+    parser.add_argument('--beeminder_json', 
                         help='https://www.beeminder.com/api/v1/auth_token.json, contains username and auth_token')
-    parser.add_argument('mongo_json', 
+    parser.add_argument('--mongo_json', 
                         help='json object contains username and password')
     parser.add_argument('--debug', action='store_true', help='is debug mode')
     args = parser.parse_args()
